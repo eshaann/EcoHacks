71605847ddd4847a1cb0c08139d87870b7cc15c0
Eshaan Nair <nair.eshaan@gmail.com>
2022-09-02 21:26:40 -0400

    initial checkin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label for = "username">Username</label>
+        <br></br>
+        <input type = "text" name = "username"></input>
+        <br></br>
+        <label for = "password">Password</label>
+        <br></br>
+        <input type = "password" name = "password"></input>
+      </form>
     </div>
   );
 }
